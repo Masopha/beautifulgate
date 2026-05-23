1828777530ed26c7c5a4f4f646279f60aed04c73
!!! Error 404 <109994277+Masopha@users.noreply.github.com>
2026-05-23 16:12:39 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,7 @@ const allowedOrigins = [
   'http://localhost:5500',
   'http://127.0.0.1:5500',
   'http://127.0.0.1:3000',
-  'https://YOUR-FRONTEND.vercel.app'   // ← replace with your Vercel URL
+  'https://beautifulgate-plum.vercel.app'  // ← replace with your Vercel URL
 ];
 
 app.use(cors({
